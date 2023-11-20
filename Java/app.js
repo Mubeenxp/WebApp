1ee4b859de7d2bd18de6c3c82d8bc3d338eeb804
Mubeenxp <mubeenxp26@gmail.com>
2023-11-20 20:58:16 +0500

    web

---
+++ Java/app.js
@@ -0,0 +1,23 @@
+//  alert("Assignment_1_JAVASCRIPTALERTS   \"ALERTS\"   \n Fisrt Class ")
+
+// console.log("Hello....I can runs JS through may web browser console")
+
+
+// var mayname = "Mubeen"
+// var lastname = "Attari"
+
+// var  fullname = mayname + lastname
+//  alert(fullname)
+
+//  var age = 10;
+//  var emailid = ("Mubeenxp26@gmail.com")
+ 
+ 
+
+//  document.write(fullname+" "+ age+  " " + " "+ lastname +""+ emailid )
+
+ var num = 10
+ console.log (++a);
+
+
+
---
+++ Java_Assignment/text.js
@@ -0,0 +1,13 @@
+
+
+var city ="karachi";
+var age = 18;
+var gender = "mail";
+
+
+if(city=="rachi" || age ==20 || gender == "mail"){
+    alert("ok")
+}else
+{
+    alert("not ok")
+}
