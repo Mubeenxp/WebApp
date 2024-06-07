fdc1c0cfd94b98e4e3d3a5a9f8672868867c3b71
Mubeenxp <mubeenxp26@gmail.com>
2024-06-07 21:38:17 +0500

    update

--- Java_Assignment/create-in_out/cv.js
+++ Java_Assignment/create-in_out/cv.js
@@ -1,5 +1,102 @@
 import { auth } from "./firbase.mjs";
 import { onAuthStateChanged  ,signOut} from " https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { db } from "./firbase.mjs";
+import { collection, addDoc} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
+
+
+
+var submit=document.getElementById('submit')
+submit.addEventListener('click', async()=>{
+  let name=document.getElementById('name').value
+  let father=document.getElementById('father').value
+  let email=document.getElementById('email').value
+  let number=document.getElementById('number').value
+  let textarea=document.getElementById('textarea').value
+  let formFileLg=document.getElementById('formFileLg').value
+  
+  let Gender = document.getElementsByName('flexRadioDefault');
+  let Gender_value;
+  for (let i = 0; i < Gender.length; i++) {
+    if (Gender[i].checked) {
+      Gender_value = Gender[i].value;
+      break;
+    }
+  }
+      
+      
+   let city=document.getElementById('select1');
+   let selectedCity=  city.options[city.selectedIndex].value;
+
+  // let Skill=document.getElementById('flexCheckChecked')
+
+  // let result = [];
+ 
+  //     result = [];
+  //     Skill.forEach(item => { // loop all the checkbox item
+  //         if (item.checked) {  //if the check box is checked
+  //             let data = {    // create an object
+  //                 item: item.value,
+  //                 selected: item.checked
+  //             }
+  //             result.push(data); //stored the objects to result array
+  //         }
+    
+  
+
+  let courses = [];
+  let courseCheckboxes = ['HTML', 'CSS', 'javascript'];
+  courseCheckboxes.forEach(function(id) {
+    let checkbox = document.getElementById(id);
+    if (checkbox.checked) {
+      courses.push(checkbox.value);
+    }
+  });
+
+
+
+
+
+  let jobdetails={
+    name:name,
+    father:father,
+    formFileLg:formFileLg,
+    email:email,
+    number:number,
+    textarea:textarea,
+    city:selectedCity,
+    Gender: Gender_value,
+    Skill:courses
+   
+   
+ 
+  }
+
+  console.log(jobdetails);
+  try {
+    const docRef = await addDoc(collection(db, "Cvbank"), {
+     ...jobdetails,
+
+     });
+    alert('form submit')
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+
+   
+  }
+
+})
+
+
+
+
+
+
+
+
+
 
 
 
@@ -34,3 +131,12 @@ onAuthStateChanged(auth, (user) => {
       
     });
   })
+
+    function getOption() {
+      selectElement =
+          document.querySelector('#select1');
+      output =
+          selectElement.options
+          [selectElement.selectedIndex].value;
+      document.querySelector('.output').textContent = output;
+  }
--- Java_Assignment/create-in_out/firbase.mjs
+++ Java_Assignment/create-in_out/firbase.mjs
@@ -2,6 +2,7 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
   import { getAuth } from " https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+  import {getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,3 +21,4 @@
   // Initialize Firebase
  export const app = initializeApp(firebaseConfig);
  export const auth = getAuth(app);
+ export const db = getFirestore(app);
